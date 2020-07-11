c221931e17892c6506a34b8ff6411426d60db9c4
Octávio Braga <octaviobraga96@gmail.com>
2020-07-11 19:14:50 +0100

    Delete flash_mem_manager.h

--- flash_mem_manager.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_FLASH_MEM_MANAGER
-#define H_FLASH_MEM_MANAGER
-
-void writeData(char addSSID, String SSID, char addPWD, String PWD, int wifiMode);
-void read_Data(char* array, char add);
-
-#endif //H_FLASH_MEM_MANAGER
