f0d29073d28419f96deb2785e275ae0c0947e1a6
Octávio Braga <octaviobraga96@gmail.com>
2020-07-11 19:15:11 +0100

    Delete node_conn_handler.h

--- node_conn_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_NODE_CONN_HANDLER
-#define H_NODE_CONN_HANDLER
-
-
-void registerDevice(String type, String location, String email);
-void postValue(int value);
-
-#endif //H_NODE_CONN_HANDLER
